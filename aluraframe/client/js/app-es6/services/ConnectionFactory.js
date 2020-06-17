c43df947bdb2c74d525a4500f2729e547500de65
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2020-06-17 00:23:39 -0300

    Trabalhando com módulos do ES2015!

--- aluraframe/client/js/app-es6/services/ConnectionFactory.js
+++ aluraframe/client/js/app-es6/services/ConnectionFactory.js
@@ -1,56 +1,54 @@
-var ConnectionFactory = (function () {
-    const stores = ['negociacoes'];
-    const version = 4;
-    const dbName = 'aluraframe';
+const stores = ['negociacoes'];
+const version = 4;
+const dbName = 'aluraframe';
 
-    var connection = null;
-    var close = null;
+let connection = null;
+let close = null;
 
-    return class ConnectionFactory {
-        constructor() {
-            throw new Error('Não é possível criar instâncias de ConnectionFactory.');
-        }
+export class ConnectionFactory {
+    constructor() {
+        throw new Error('Não é possível criar instâncias de ConnectionFactory.');
+    }
 
-        static getConnection() {
-            return new Promise((resolve, reject) => {
-                let openRequest = window.indexedDB.open(dbName, version);
+    static getConnection() {
+        return new Promise((resolve, reject) => {
+            let openRequest = window.indexedDB.open(dbName, version);
 
-                openRequest.onupgradeneeded = e => {
-                    ConnectionFactory._createStore(e.target.result);
-                };
+            openRequest.onupgradeneeded = e => {
+                ConnectionFactory._createStore(e.target.result);
+            };
 
-                openRequest.onsuccess = e => {
-                    if (!connection) {
-                        connection = e.target.result;
-                        close = connection.close.bind(connection);
-                        connection.close = function () {
-                            throw new Error('Você não pode fechar diretamente a conexão.');
-                        };
-                    }
-
-                    resolve(connection);
-                };
-
-                openRequest.onerror = e => {
-                    reject(e.target.error.name);
-                };
-            });
-        }
-
-        static _createStore(connection) {
-            stores.forEach(store => {
-                if (connection.objectStoreNames.contains(store)) {
-                    connection.deleteObjectStore(store);
+            openRequest.onsuccess = e => {
+                if (!connection) {
+                    connection = e.target.result;
+                    close = connection.close.bind(connection);
+                    connection.close = function () {
+                        throw new Error('Você não pode fechar diretamente a conexão.');
+                    };
                 }
-                connection.createObjectStore(store, { autoIncrement: true });
-            });
-        }
 
-        static closeConnection() {
-            if (connection) {
-                close();
-                connection = null;
+                resolve(connection);
+            };
+
+            openRequest.onerror = e => {
+                reject(e.target.error.name);
+            };
+        });
+    }
+
+    static _createStore(connection) {
+        stores.forEach(store => {
+            if (connection.objectStoreNames.contains(store)) {
+                connection.deleteObjectStore(store);
             }
+            connection.createObjectStore(store, {autoIncrement: true});
+        });
+    }
+
+    static closeConnection() {
+        if (connection) {
+            close();
+            connection = null;
         }
     }
-})();
+}
--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,58 +1,92 @@
 "use strict";
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+System.register([], function (_export, _context) {
+    "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+    var _createClass, Negociacao;
 
-var Negociacao = function () {
-    function Negociacao(data, quantidade, valor) {
-        _classCallCheck(this, Negociacao);
-
-        this._data = new Date(data.getTime());
-        this._quantidade = quantidade;
-        this._valor = valor;
-
-        Object.freeze(this);
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
     }
 
-    _createClass(Negociacao, [{
-        key: "isEquals",
-        value: function isEquals(outraNegociacao) {
-            return this._data.getTime() === outraNegociacao.data.getTime() && this._quantidade === outraNegociacao.quantidade && this._valor === outraNegociacao.valor;
-        }
-    }, {
-        key: "volume",
-        get: function get() {
-            return this._quantidade * this._valor;
-        }
-    }, {
-        key: "data",
-        get: function get() {
-            return new Date(this._data.getTime());
-        }
-    }, {
-        key: "quantidade",
-        get: function get() {
-            return this._quantidade;
-        }
-    }, {
-        key: "valor",
-        get: function get() {
-            return this._valor;
-        }
-    }], [{
-        key: "create",
-        value: function create(_ref) {
-            var _ref$data = _ref.data,
-                data = _ref$data === undefined ? new Date() : _ref$data,
-                _ref$quantidade = _ref.quantidade,
-                quantidade = _ref$quantidade === undefined ? 1 : _ref$quantidade,
-                _ref$valor = _ref.valor,
-                valor = _ref$valor === undefined ? 1.0 : _ref$valor;
+    return {
+        setters: [],
+        execute: function () {
+            _createClass = function () {
+                function defineProperties(target, props) {
+                    for (var i = 0; i < props.length; i++) {
+                        var descriptor = props[i];
+                        descriptor.enumerable = descriptor.enumerable || false;
+                        descriptor.configurable = true;
+                        if ("value" in descriptor) descriptor.writable = true;
+                        Object.defineProperty(target, descriptor.key, descriptor);
+                    }
+                }
 
-            return new Negociacao(data, quantidade, valor);
-        }
-    }]);
+                return function (Constructor, protoProps, staticProps) {
+                    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+                    if (staticProps) defineProperties(Constructor, staticProps);
+                    return Constructor;
+                };
+            }();
 
-    return Negociacao;
-}();
+            _export("Negociacao", Negociacao = function () {
+                function Negociacao(data, quantidade, valor) {
+                    _classCallCheck(this, Negociacao);
+
+                    this._data = new Date(data.getTime());
+                    this._quantidade = quantidade;
+                    this._valor = valor;
+
+                    Object.freeze(this);
+                }
+
+                _createClass(Negociacao, [{
+                    key: "isEquals",
+                    value: function isEquals(outraNegociacao) {
+                        return this._data.getTime() === outraNegociacao.data.getTime() && this._quantidade === outraNegociacao.quantidade && this._valor === outraNegociacao.valor;
+                    }
+                }, {
+                    key: "volume",
+                    get: function get() {
+                        return this._quantidade * this._valor;
+                    }
+                }, {
+                    key: "data",
+                    get: function get() {
+                        return new Date(this._data.getTime());
+                    }
+                }, {
+                    key: "quantidade",
+                    get: function get() {
+                        return this._quantidade;
+                    }
+                }, {
+                    key: "valor",
+                    get: function get() {
+                        return this._valor;
+                    }
+                }], [{
+                    key: "create",
+                    value: function create(_ref) {
+                        var _ref$data = _ref.data,
+                            data = _ref$data === undefined ? new Date() : _ref$data,
+                            _ref$quantidade = _ref.quantidade,
+                            quantidade = _ref$quantidade === undefined ? 1 : _ref$quantidade,
+                            _ref$valor = _ref.valor,
+                            valor = _ref$valor === undefined ? 1.0 : _ref$valor;
+
+                        return new Negociacao(data, quantidade, valor);
+                    }
+                }]);
+
+                return Negociacao;
+            }());
+
+            _export("Negociacao", Negociacao);
+        }
+    };
+});
+//# sourceMappingURL=Negociacao.js.map
