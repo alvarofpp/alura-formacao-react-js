f8d36424199c1e68ad37a89783d844fd5c9058b9
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2020-06-22 18:37:18 -0300

    Webpack Dev Server e configuração

--- projeto-webpack/client/app-src/domain/negociacao/NegociacaoService.js
+++ projeto-webpack/client/app-src/domain/negociacao/NegociacaoService.js
@@ -5,14 +5,12 @@ import { ApplicationException } from '../../util/ApplicationException.js';
 export class NegociacaoService {
 
     constructor() {
-
         this._http = new HttpService();
     }
 
     obtemNegociacoesDaSemana() {
-
         return this._http
-            .get('negociacoes/semana')
+            .get('http//localhost:3000/negociacoes/semana')
             .then(
             dados =>
                 dados.map(objeto =>
@@ -26,9 +24,8 @@ export class NegociacaoService {
     }
 
     obtemNegociacoesDaSemanaAnterior() {
-
         return this._http
-            .get('negociacoes/anterior')
+            .get('http//localhost:3000/negociacoes/anterior')
             .then(
             dados => dados.map(objeto =>
                 new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
@@ -41,9 +38,8 @@ export class NegociacaoService {
     }
 
     obtemNegociacoesDaSemanaRetrasada() {
-
         return this._http
-            .get('negociacoes/retrasada')
+            .get('http//localhost:3000/negociacoes/retrasada')
             .then(
             dados => dados.map(objeto =>
                 new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
@@ -55,7 +51,6 @@ export class NegociacaoService {
     }
 
     async obtemNegociacoesDoPeriodo() {
-
         try {
             let periodo = await Promise.all([
                 this.obtemNegociacoesDaSemana(),
--- projeto-webpack/client/webpack.config.js
+++ projeto-webpack/client/webpack.config.js
@@ -10,8 +10,9 @@ if(process.env.NODE_ENV === 'production') {
 module.exports = {
     entry: './app-src/app.js',
     output: {
-        'filename': 'bundle.js',
-        'path': path.resolve(__dirname, 'dist')
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'dist'
     },
     module: {
         rules: [
