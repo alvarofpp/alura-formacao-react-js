0ca633ad041f8ea37d6780b8e9bc04ce9c48f8d4
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2020-06-22 22:14:08 -0300

    Usando componentes

--- curso-react-alura/src/App.js
+++ curso-react-alura/src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
+import Tabela from './Tabela';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+    const  autores =  [
+        {
+            nome: 'Paulo',
+            livro: 'React',
+            preco: '1000'
+        },
+        {
+            nome: 'Daniel',
+            livro: 'Java',
+            preco: '99'
+        },
+        {
+            nome: 'Marcos',
+            livro: 'Design',
+            preco: '150'
+        },
+        {
+            nome: 'Bruno',
+            livro: 'DevOps',
+            preco: '100'
+        }
+    ];
+
+    return (
+        <div className="App">
+            <Tabela autores={autores}/>
+        </div>
+    );
 }
 
 export default App;
